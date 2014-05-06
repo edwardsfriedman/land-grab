3d8913fd9b57d746258f46602269ef93493446e1
Eddie <edward.s.friedman@gmail.com>
2014-05-06 12:51:59 -0400

    include optional mapbounds in search

--- public/map.js
+++ public/map.js
@@ -1,5 +1,11 @@
+/** Prelimenary setup: loads map, calls for data
+ * to populate dropdowns in search, populates
+ * dropdowns, expands search window.
+ */
 window.addEventListener('load', function(){
- /****** MAP SETUP *******/
+  /****** ACTION BOX *******/
+  document.getElementById('actionBox').style.display = "none";
+  /****** MAP SETUP *******/
   var bounds = new L.LatLngBounds(new L.LatLng(85.00542734823001, 214.62890625),
            new L.LatLng(-85.035941506574, -194.4140625));
   map = L.mapbox.map('map', 'friedboy.hml0l3kn', {
@@ -9,11 +15,11 @@ window.addEventListener('load', function(){
   map.zoomControl.setPosition('topright');
   map.scrollWheelZoom.disable();
 
-  //***GEOCODER*****/
+  //***GEOCODER, FEATURELAYER*****/
   geodude = L.mapbox.geocoder('friedboy.hml0l3kn');
-
   featureLayer = L.mapbox.featureLayer();
   /****** Load DATA ******/
+  //request for data to populate drops
   var req = new XMLHttpRequest();
   req.open('GET', '/populateMap.json', true);
   req.addEventListener('load', function(e){
@@ -22,10 +28,10 @@ window.addEventListener('load', function(){
         window.alert(e.srcElement.response);
         return;
     }
-    names = [];
-    locations = [];
-    grabbers = [];
-    resistance = [];
+    var names = [];
+    var locations = [];
+    var grabbers = [];
+    var resistance = [];
     var content = req.responseText;
     var mydata = JSON.parse(content);
     var datalen = mydata.length
@@ -34,55 +40,55 @@ window.addEventListener('load', function(){
     var resistsublen;
     var j;
     for (i=0; i<datalen; i++){
-        //grab names
-        if ($.inArray(mydata[i].name, names) == -1){
-          console.log('adding name');
-          names.push(mydata[i].name);
-        }
-        //grab locations
-        if ($.inArray(mydata[i].city, locations) == -1){
-          locations.push(mydata[i].city);
-        }
-        //grab offenders
-        if (mydata[i].grabbers){
-          grabsublen = mydata[i].grabbers.length;
-          for (j=0; j<grabsublen; j++){
-            if ($.inArray(mydata[i].grabbers[j], grabbers) == -1){
-              grabbers.push(mydata[i].grabbers[j]);
-            }
-          }
-        }
-        //grab resistance
-        if (mydata[i].resistance){
-          resistsublen = mydata[i].resistance.length;
-          for (j=0; j<resistsublen; j++){
-            if ($.inArray(mydata[i].resistance[j], resistance) == -1){
-              resistance.push(mydata[i].resistance[j]);
-            }
+      //grab names
+      if ($.inArray(mydata[i].name, names) == -1){
+        names.push(mydata[i].name);
+      }
+      //grab locations
+      if ($.inArray(mydata[i].city, locations) == -1){
+        locations.push(mydata[i].city);
+      }
+      //grab offenders
+      if (mydata[i].grabbers){
+        grabsublen = mydata[i].grabbers.length;
+        for (j=0; j<grabsublen; j++){
+          if ($.inArray(mydata[i].grabbers[j], grabbers) == -1){
+            grabbers.push(mydata[i].grabbers[j]);
           }
         }
       }
-      populateDrops();
-      srchButton();
-    });
-    req.send();
-
-  /****** SEARCH BOX *******/
-  document.getElementById('actionBox').style.display = "none";
+      //grab resistance
+      if (mydata[i].resistance){
+        resistsublen = mydata[i].resistance.length;
+        for (j=0; j<resistsublen; j++){
+          if ($.inArray(mydata[i].resistance[j], resistance) == -1){
+            resistance.push(mydata[i].resistance[j]);
+          }
+        }
+      }
+    }
+    //loads data into dropdowns
+    populateDrops(names, locations, grabbers, resistance);
+    //expands search button when dropdowns are populated
+    srchButton();
+  });
+  req.send();
 }, false);
 
-function populateDrops(){
+/** Passed arrays of names, locations, grabbers, resistance.
+ *  Adds data to respective dropdown menus.
+ */
+function populateDrops(names, locations, grabbers, resistance){
   var nameDrop = document.getElementById("nameDrop").children[2];
   var locDrop = document.getElementById("locDrop").children[2];
   var grabDrop = document.getElementById("entitiesDrop").children[2];
   var resistDrop = document.getElementById("resistanceDrop").children[2];
-  //Put data in list
   var nameslen = names.length;
-  console.log(nameslen + " is how many names");
   var loclen = locations.length;
   var grablen = grabbers.length;
   var resistlen = resistance.length;
   var i;
+  //Put data in list
   for (i=0; i < nameslen; i++) {
     option = document.createElement('option');
     option.value = names[i];
@@ -107,9 +113,11 @@ function populateDrops(){
     option.innerHTML = resistance[i];
     resistDrop.appendChild(option);
   }
-
 }
 
+/** Onclick function for 'search' button beside header.
+ *  Hides or displays actionbox's contents accordingly.
+ */
 function srchButton(){
   var resultsBox = document.getElementById('resultsContain');
   var actionBox = document.getElementById('actionBox');
@@ -145,6 +153,9 @@ function srchButton(){
   }
 };
 
+/** Onclick function for 'share' button beside header.
+ *  Hides or displays actionbox's contents accordingly.
+ */
 function shareButton(){
     var resultsBox = document.getElementById('resultsContain');
     var actionBox = document.getElementById('actionBox');
@@ -291,8 +302,10 @@ function doSearch() {
     var location = collectSelections("locDrop");
     var grabbers = collectSelections("entitiesDrop");
     var resistance = collectSelections("resistanceDrop");
+    var geobox = document.getElementById('geosearch');
 
     var url = document.URL + "/search.json";
+    //call back for creating search results
     var cb = function(data){
       resultCont.innerHTML = "<span class='bolder'>results:</span> <button class='container' type='button' id='resultsBack' onclick='back()'> <-- </button><br>";
       /** geoJSON **/
@@ -331,31 +344,39 @@ function doSearch() {
     buildForm(location, "location", fd);
     buildForm(grabbers, "grabbers", fd);
     buildForm(resistance, "resistance", fd);
+    // if geo-bounded search: grab bounds from map and include them in form;
+    if (geobox.checked){
+      var bounds = map.getBounds();
+      var boundsobj = {};
+      boundsobj.ne = {lat: bounds.getNorthEast().lat, lng: bounds.getNorthEast().lng};
+      boundsobj.nw = {lat: bounds.getNorthWest().lat, lng: bounds.getNorthWest().lng};
+      boundsobj.sw = {lat: bounds.getSouthWest().lat, lng: bounds.getSouthWest().lng};
+      boundsobj.se = {lat: bounds.getSouthEast().lat, lng: bounds.getSouthEast().lng};
+      fd.append('bounds', JSON.stringify(boundsobj));
+    }
     // send it to the server
     var req = new XMLHttpRequest();
     req.open('POST', '/search.json', true);
     req.addEventListener('load', function(e){
-        //error_handle
-        if(e.currentTarget.status != 200){
-            window.alert(e.srcElement.response);
-            return;
-        }
-      $(actionBox).slideToggle(function(){
-        map.setView([55, 10], 2);
-        document.getElementById("searchBox").style.display = 'none';
-        resultCont.style.display = 'block';
-        var content = req.responseText;
-        var data = JSON.parse(content);
-        cb(data);
-        if (resultCont.innerHTML===""){
-          resultCont.innerHTML = "No Results Matched Your Search";
-          //$(resultsContain).slideToggle();
-        }
-        if(resultCont.style.display === "none" || resultCont.style.display ==="block"){
-          //$(resultsContain).slideToggle();
-        }
-        $(actionBox).slideToggle();
-      });
+      //error_handle
+      if(e.currentTarget.status != 200){
+          window.alert(e.srcElement.response);
+          return;
+      } else {
+        //hide box, then load content;
+        $(actionBox).slideToggle(function(){
+          map.setView([55, 10], 2);
+          document.getElementById("searchBox").style.display = 'none';
+          resultCont.style.display = 'block';
+          var content = req.responseText;
+          var data = JSON.parse(content);
+          cb(data);
+          if(resultCont.style.display === "none" || resultCont.style.display ==="block"){
+            //$(resultsContain).slideToggle();
+          }
+          $(actionBox).slideToggle();
+        });
+      }
     }, false);
     req.send(fd);
 }
@@ -505,7 +526,7 @@ function buildResult(url, name, descrip, grabs, resists, location){
     resString += ", ";
   }
   inner = "<p>" + name + "<br>"+ location +"</p>"+
-  "<div class=fullResult style='display: none'>" +
+  "<div class=fullResult style='display: none's>" +
   "<p><span class='under'>Description</span>: " + descrip + "</p>" +
   "<p><span class='under'>url</span>:   <a href='" + url + "' target='_blank'>" + url.split('/')[2] + "</a></p>" +
   "<p><span class='under'>Culpable governments, companies & individuals:</span> " + grabberString + "</p>" +
